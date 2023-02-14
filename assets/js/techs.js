a85dbd2da885824b1fd7f83e93f876558ed86e37
Bruno <32647263+ybruno0709@users.noreply.github.com>
2023-02-14 18:01:00 -0300

    little details in techs section

--- assets/js/techs.js
+++ assets/js/techs.js
@@ -7,7 +7,8 @@ $(() => {
     let current = 0;
 
 
-    const colors = ["#e44d26", "#3492cb", "#ead41c", "#0863a2"];
+    const colorsDefault = ["#e44d26", "#3492cb", "#ead41c", "#0863a2"];
+    const colorsActive = ["#ff572d", "#57b8f5", "#ffeb38", "#2c97e4"];
     const technologies = ["HTML 5", "CSS 3", "JavaScript", "jQuery"];
     const descriptionArr = ["HTML (Hypertext Markup Language) é uma linguagem de marcação usada para criar páginas da web. Ela é usada para estruturar o conteúdo da página da web, incluindo texto, imagens, links, vídeos e outros tipos de mídia",
         "CSS (Cascading Style Sheets) é uma linguagem de folha de estilo usada para definir a aparência e o layout de elementos HTML em uma página da web. Em outras palavras, o CSS é usado para separar a estrutura e conteúdo de uma página da web de sua apresentação visual",
@@ -18,21 +19,9 @@ $(() => {
     animation();
 
     let delay = setInterval(() => {
-        animation();
         checkCurrent();
+        animation();
     }, 1250);
-
-    cubes.each(i => {
-        $(cubes[i]).click(() => {
-            current = i;
-            $(cubes[i]).addClass('active');
-
-            cubes.removeClass('active');
-
-            animation();
-            clearInterval(delay);
-        });
-    });
 
     dropdownBtn.click(() => {
         let actualClass = $(dropdownSymbol).attr('class').split(' ')[1];
@@ -66,17 +55,63 @@ $(() => {
         });
     }
 
-    function textTransform() {
-        technology.html(technologies[current]);
-        technology.css('color', colors[current]);
-    }
-
     function changeDescription() {
         descriptionEl.html(descriptionArr[current]);
     }
 
+    function changeCubeColor() {
+        cubes.each(i => {
+            const actual = $(cubes[i]).attr('id').split('-')[1] * 1;
+
+            $(cubes[i]).mousedown(() => {
+                $(cubes[i]).find('div').css('background-color', colorsActive[actual]);
+            });
+
+            $(cubes[i]).mouseup(() => {
+                $(cubes[i]).find('div').css('background-color', colorsDefault[actual]);
+            });
+        });
+    }
+
+    function changeFaceColor() {
+        cubes.each(i => {
+            const actual = $(cubes[i]).attr('id').split('-')[1] * 1;
+            const faces = $(cubes[i]).find('div');
+
+            faces.each(idx => {
+                $(faces[idx]).hover(() => {
+                    $(faces[idx]).css('background-color', colorsActive[actual]);
+                }, () => {
+                    $(faces[idx]).css('background-color', colorsDefault[actual]);
+                });
+            });
+        });
+    }
+
+    function cubeClick() {
+        cubes.each(i => {
+            $(cubes[i]).click(() => {
+                current = i;
+                $(cubes[i]).addClass('active');
+
+                cubes.removeClass('active');
+
+                animation();
+                clearInterval(delay);
+            });
+        });
+    }
+
+    function textTransform() {
+        technology.html(technologies[current]);
+        technology.css('color', colorsDefault[current]);
+    }
+
     function animation() {
+        cubeClick();
         changeActiveCube();
+        changeCubeColor();
+        changeFaceColor();
         changeDescription();
         textTransform();
     }
